6d1995372296249a5df442cb81c6dcb2b382c465
Moh Sameer Khan <96594154+Moh-Sameer-Khan@users.noreply.github.com>
2022-03-06 19:36:12 +0530

    upload

---
+++ March/Test06March/Code/index.js
@@ -0,0 +1,21 @@
+function showPopUp(id) {
+    var show = document.getElementById('subscribe');
+    show.classList.add('d-show');
+}
+function hidePopUp(id) {
+    var hide =  document.getElementById('subscribe');
+    hide.classList.remove('d-show');   
+}
+function showPopUp5(id) {
+    var show = document.getElementById('subscribe5');
+    show.classList.add('d-show');
+}
+function hidePopUp5(id) {
+    var hide =  document.getElementById('subscribe5');
+    hide.classList.remove('d-show');   
+}
+
+
+
+
+
